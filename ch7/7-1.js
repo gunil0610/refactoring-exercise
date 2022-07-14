fdeaecec2453dbde7ec2602755ae7f9a72898bc3
gunil0610 <gunil.na1@gmail.com>
2022-07-14 17:36:30 +0900

    refactor: 7-1 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,38 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+import _ from "lodash";
 
-organization.name = 'Dream Coding';
+class Organization {
+  #name;
+  #country;
+  #data;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  get rawData() {
+    return _.cloneDeep(this.#data);
+  }
+}
+
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
+console.log(organization.rawData);
