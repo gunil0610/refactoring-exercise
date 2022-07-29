e6d752e386730493cc4c4e7977dd51789fe0d9ef
gunil0610 <gunil.na1@gmail.com>
2022-07-29 14:21:05 +0900

    refactor: 7-3 기본형을 객체로 바꾸기2

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -4,14 +4,46 @@ export class Order {
   }
 
   isHighPriority() {
-    return "high" === this.priority || "rush" === this.priority;
+    return this.priority.higherThan(new Priority("normal"));
+  }
+}
+
+// 필요한 경우에만!
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  lowerThan(other) {
+    return this.index < other.index;
+  }
+
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
   }
 }
 
 const orders = [
-  new Order({ priority: "normal" }),
-  new Order({ priority: "high" }),
-  new Order({ priority: "rush" }),
+  new Order({ priority: new Priority("normal") }),
+  new Order({ priority: new Priority("high") }),
+  new Order({ priority: new Priority("rush") }),
 ];
 
 const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
