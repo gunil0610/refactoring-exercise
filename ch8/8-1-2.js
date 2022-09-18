ce5259cd44f553d91965e86c66cfdd9a78976fa3
gunil0610 <gunil.na1@gmail.com>
2022-09-19 00:37:24 +0900

    refactor: 8-1-2 overdraftCharge 추가 리팩토링

--- ch8/8-1-2.js
+++ ch8/8-1-2.js
@@ -25,15 +25,12 @@ export class AccountType {
   }
 
   overdraftCharge(daysOverdrawn) {
-    if (this.isPremium) {
-      const baseCharge = 10;
-      if (daysOverdrawn <= 7) {
-        return baseCharge;
-      } else {
-        return baseCharge + (daysOverdrawn - 7) * 0.85;
-      }
-    } else {
+    if (!this.isPremium) {
       return daysOverdrawn * 1.75;
     }
+    const baseCharge = 10;
+    return daysOverdrawn <= 7
+      ? baseCharge
+      : baseCharge + (daysOverdrawn - 7) * 0.85;
   }
 }
