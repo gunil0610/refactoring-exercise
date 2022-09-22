2ada361d1acc45a5d3e48397d7f9f54b69582b3d
gunil0610 <gunil.na1@gmail.com>
2022-09-22 23:45:11 +0900

    refactor: 10-6 어서션 추가하기

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,13 @@
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0); // 개발 단계에서만 하고, production에서는 로그만 남기고 정상동작 하도록 하는게 좋다
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(-1);
