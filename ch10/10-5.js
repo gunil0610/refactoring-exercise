9056c7c8161598c9b060f19fabc993431946a887
gunil0610 <gunil.na1@gmail.com>
2022-09-22 23:37:31 +0900

    refactor: 10-5 특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,10 +1,53 @@
+class Hotel {
+  constructor() {
+    this.rooms = [];
+  }
+
+  addRoom(roomNumber) {
+    this.rooms[roomNumber] = new Room(roomNumber);
+  }
+
+  emptyRoom(roomNumber) {
+    // this.rooms[roomNumber] = null; // BAD
+    this.rooms[roomNumber] = new EmptyRoom(roomNumber); // BAD
+  }
+
+  cleanRooms() {
+    this.rooms.forEach((room) => room.clean());
+  }
+}
+
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  clean() {
+    console.log("clean");
+  }
+}
+
+class EmptyRoom extends Room {
+  clean() {
+    console.log("it's empty");
+  }
+}
+
 export class Site {
   constructor(customer) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === "unknown"
+      ? new UnknownCustomer()
+      : new Customer(this._customer);
+  }
+}
+
+class UnknownCustomer extends Customer {
+  get name() {
+    return "occupant";
   }
 }
 
@@ -34,9 +77,7 @@ export class Customer {
 export function customerName(site) {
   const aCustomer = site.customer;
   // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+  let customerName = aCustomer.name;
 
   return customerName;
 }
