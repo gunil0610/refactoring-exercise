651605d147c5a8866e57c6ab23c7581679d30ea1
gunil0610 <gunil.na1@gmail.com>
2022-09-26 18:11:42 +0900

    refactor: 11-3 플래그 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -1,19 +1,20 @@
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function setWidth(value){
+  this._height = value;
+}
+
+function setHeight(value){
+  this._width = value;
 }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {}
+  premiumBook(customer) {}
+  // 내부적으로 중복되는 로직이 많다면, 이렇게 내부에서만 사용하도록 해도 OK
+  #book(customer, isPremium){}
 }
 
 // 예제 3
-function setSwitch(on);
+function switchOn();
+function switchOff();
